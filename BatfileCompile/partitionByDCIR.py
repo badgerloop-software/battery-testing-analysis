e98928a8d8ea0866f3a3520b1f8872a854e16ed6
James <jrvollmer@wisc.edu>
2022-11-02 00:49:48 -0500

    Specify partititon sizes/numbers in CLAs and added avg DCIR to data listed

--- BatfileCompile/partitionByDCIR.py
+++ BatfileCompile/partitionByDCIR.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
@@ -18,35 +19,32 @@ def sort_and_partition_data(sort_expression, sort_first, sort_second, n_groups, 
     :param group_size: The size of the partitions to make
     """
 
-    battery_info = {
-        'name': [],
-        'dcir1': [],
-        'dcir2': [],
-        'box': [],
-        'ocv': [],
-        'capacity': []
-    }
     battery_info_arr = []
 
     wb = pd.read_excel(io='Individual_Data.xlsx', sheet_name='Sheet1')
 
-    # Clumsily put name, dcir1, dcir2, box, ocv, and capacity into an array with object elements
-    battery_info['name'] = list(wb['Cell'].to_numpy())
-    battery_info['dcir1'] = list(wb['DCIR 1 (Idle-P1)'].to_numpy())
-    battery_info['dcir2'] = list(wb['DCIR 2 (P1-P2)'].to_numpy())
-    battery_info['box'] = list(wb['Box'].to_numpy())
-    battery_info['ocv'] = list(wb['OCV'].to_numpy())
-    battery_info['capacity'] = list(wb['Capacity'].to_numpy())
+    # Put name, dcir1, dcir2, box, ocv, and capacity into an array with object elements
+    names = list(wb['Cell'].to_numpy())
+    dcir1s = list(wb['DCIR 1 (Idle-P1)'].to_numpy())
+    dcir2s = list(wb['DCIR 2 (P1-P2)'].to_numpy())
+    boxes = list(wb['Box'].to_numpy())
+    ocvs = list(wb['OCV'].to_numpy())
+    capacities = list(wb['Capacity'].to_numpy())
 
-    for i in range(len(battery_info['name'])):
+    for i in range(len(names)):
         battery_info_arr.append({
-            'name': battery_info['name'][i],
-            'dcir1': battery_info['dcir1'][i],
-            'dcir2': battery_info['dcir2'][i],
-            'box': battery_info['box'][i],
-            'ocv': battery_info['ocv'][i],
-            'capacity': battery_info['capacity'][i]
+            'name': names[i],
+            'dcir1': dcir1s[i],
+            'dcir2': dcir2s[i],
+            'box': boxes[i],
+            'ocv': ocvs[i],
+            'capacity': capacities[i]
         })
+
+    # Make sure there are enough cells
+    if((n_groups * group_size) > len(names)):
+        print('The number of cells expecting to be partitioned (num_modules * cells_per_group) is greater than the number of available cells')
+        return
 
     # Filter out nan from battery_info_arr because sort doesn't like them
     filter_arr = []
@@ -67,12 +65,6 @@ def sort_and_partition_data(sort_expression, sort_first, sort_second, n_groups, 
     k, m = divmod(len(cut_down_bat_info_arr), n_groups)
     partitioned_bat_info = list(cut_down_bat_info_arr[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n_groups))
 
-    for i in range(len(partitioned_bat_info)):
-        print('Length: ' + str(len(partitioned_bat_info[i])) +
-              '\tExtreme Diff: ' + str(partitioned_bat_info[i][-1]['dcir1'] - partitioned_bat_info[i][0]['dcir1']) +
-              '\n\tFirst: ' + str(partitioned_bat_info[i][0]['dcir1']) + ' / ' + str(partitioned_bat_info[i][0]['dcir2']) +
-              '\n\tSecond: ' + str(partitioned_bat_info[i][-1]['dcir1']) + ' / ' + str(partitioned_bat_info[i][-1]['dcir2']))
-
     # Create/Load workbook to put partitioned cells into
     try:
         partitioned_wb = load_workbook(filename='Partitioned_Data.xlsx')
@@ -88,29 +80,39 @@ def sort_and_partition_data(sort_expression, sort_first, sort_second, n_groups, 
 
     # Add the partitioned cells and their DCIR values into the workbook
     for i in range(len(partitioned_bat_info)):
-        partitioned_wb.active.cell(row=1, column=i*7 + 1).value = 'Group ' + str(i+1)
-        partitioned_wb.active.cell(row=2, column=i*7 + 1).value = 'Cell'
-        partitioned_wb.active.cell(row=2, column=i*7 + 2).value = 'Box'
-        partitioned_wb.active.cell(row=2, column=i*7 + 3).value = 'OCV'
-        partitioned_wb.active.cell(row=2, column=i*7 + 4).value = 'Capacity'
-        partitioned_wb.active.cell(row=2, column=i*7 + 5).value = 'DCIR 1 (Idle-P1)'
-        partitioned_wb.active.cell(row=2, column=i*7 + 6).value = 'DCIR 2 (P1-P2)'
+        partitioned_wb.active.cell(row=1, column=i*8 + 1).value = 'Group ' + str(i+1)
+        partitioned_wb.active.cell(row=2, column=i*8 + 1).value = 'Cell'
+        partitioned_wb.active.cell(row=2, column=i*8 + 2).value = 'Box'
+        partitioned_wb.active.cell(row=2, column=i*8 + 3).value = 'OCV'
+        partitioned_wb.active.cell(row=2, column=i*8 + 4).value = 'Capacity'
+        partitioned_wb.active.cell(row=2, column=i*8 + 5).value = 'DCIR 1 (Idle-P1)'
+        partitioned_wb.active.cell(row=2, column=i*8 + 6).value = 'DCIR 2 (P1-P2)'
+        partitioned_wb.active.cell(row=2, column=i*8 + 7).value = 'Avg(DCIR1, DCIR2)'
         for j in range(len(partitioned_bat_info[i])):
-            partitioned_wb.active.cell(row=j+3, column=i*7 + 1).value = partitioned_bat_info[i][j]['name']
-            partitioned_wb.active.cell(row=j+3, column=i*7 + 2).value = partitioned_bat_info[i][j]['box']
-            partitioned_wb.active.cell(row=j+3, column=i*7 + 3).value = partitioned_bat_info[i][j]['ocv']
-            partitioned_wb.active.cell(row=j+3, column=i*7 + 4).value = partitioned_bat_info[i][j]['capacity']
-            partitioned_wb.active.cell(row=j+3, column=i*7 + 5).value = partitioned_bat_info[i][j]['dcir1']
-            partitioned_wb.active.cell(row=j+3, column=i*7 + 6).value = partitioned_bat_info[i][j]['dcir2']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 1).value = partitioned_bat_info[i][j]['name']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 2).value = partitioned_bat_info[i][j]['box']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 3).value = partitioned_bat_info[i][j]['ocv']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 4).value = partitioned_bat_info[i][j]['capacity']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 5).value = partitioned_bat_info[i][j]['dcir1']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 6).value = partitioned_bat_info[i][j]['dcir2']
+            partitioned_wb.active.cell(row=j+3, column=i*8 + 7).value = (partitioned_bat_info[i][j]['dcir1'] + partitioned_bat_info[i][j]['dcir2']) / 2
 
     partitioned_wb.save('Partitioned_Data.xlsx')
 
 
 # Main function
 if __name__ == '__main__':
-    numModules = 30
-    cellsInModule = 15
+    if(len(sys.argv) != 3):
+        print('Usage: `py partitionByDCIR.py <number_of_modules> <cells_per_module>`')
+    else:
+        try:
+            # Get number of modules and cells per module from CLAs
+            numModules = int(sys.argv[1])
+            cellsInModule = int(sys.argv[2])
 
-    sort_and_partition_data(lambda x: (x['dcir1'], x['dcir2']), 'DCIR 1', 'DCIR 2', numModules, cellsInModule)
-    sort_and_partition_data(lambda x: (x['dcir2'], x['dcir1']), 'DCIR 2', 'DCIR 1', numModules, cellsInModule)
-    sort_and_partition_data(lambda x: (x['dcir1'] + x['dcir2']) / 2, 'avg(DCIR 1, DCIR 2)', '', numModules, cellsInModule)
+            # Partition cells using 3 different criteria
+            sort_and_partition_data(lambda x: (x['dcir1'], x['dcir2']), 'DCIR 1', 'DCIR 2', numModules, cellsInModule)
+            sort_and_partition_data(lambda x: (x['dcir2'], x['dcir1']), 'DCIR 2', 'DCIR 1', numModules, cellsInModule)
+            sort_and_partition_data(lambda x: (x['dcir1'] + x['dcir2']) / 2, 'avg(DCIR 1, DCIR 2)', '', numModules, cellsInModule)
+        except ValueError:
+            print('Usage: `py partitionByDCIR.py <number of modules> <cells per module>`')
